de4ea5c7bb26cbff40955f9a520b279528ef56ae
jenishnj <65988674+jenishnj@users.noreply.github.com>
2022-11-01 16:31:55 +0530

    Update create-org.sh

--- scripts/create-org.sh
+++ scripts/create-org.sh
@@ -2,14 +2,14 @@
 echo 'Hello World'
 echo $@
 
-userDetails=$(sfdx force:org:create -f config/project-scratch-def.json -a $@ -s --json -d 1 -w 10)
-LOGIN_USERNAME=$(echo $userDetails | jq -r .result.username)
+LOGIN_USERNAME='test-xr8i0fahlfqc@example.com'
 
 # Generate password
 sfdx force:user:password:generate --targetusername $LOGIN_USERNAME
 
 # Get scratch org details
 orgDetails=$(sfdx force:org:display -u $LOGIN_USERNAME --json)
+echo $orgDetails | grep -i password
 LOGIN_PASSWORD=$(echo $orgDetails | grep -i password | awk '{print $2}' | sed 's/[",]//g')
 LOGIN_INSTANCE_URL=$(echo $orgDetails | grep -i instanceUrl | awk '{print $2}' | sed 's/[",]//g')
 LOGIN_ORG_ID=$(echo $orgDetails | grep -i id | awk 'NR==1{print $2}' | sed 's/[",]//g')
