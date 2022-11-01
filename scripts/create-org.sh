fcc7c3de56f2a68fb75c48d7f7e67182097ba99c
jenishnj <65988674+jenishnj@users.noreply.github.com>
2022-11-01 17:14:12 +0530

    Update create-org.sh

--- scripts/create-org.sh
+++ scripts/create-org.sh
@@ -13,3 +13,8 @@ LOGIN_USERNAME='test-xr8i0fahlfqc@example.com'
 LOGIN_PASSWORD=$(cat temp.json | awk '/password/ {print $2}' | sed 's/[",]//g')
 LOGIN_INSTANCE_URL=$(cat temp.json | awk '/instanceUrl/ {print $2}' | sed 's/[",]//g')
 LOGIN_ORG_ID=$(cat temp.json | awk '/id/ {print $2;exit}' | sed 's/[",]//g')
+echo "LOGIN_ORG_ID=$LOGIN_ORG_ID" >> .env
+echo "LOGIN_INSTANCE_URL=$LOGIN_INSTANCE_URL" >> .env
+echo "LOGIN_USERNAME=$LOGIN_USERNAME" >> .env
+echo "LOGIN_PASSWORD=$LOGIN_PASSWORD" >> .env
+rm -rf temp.json
