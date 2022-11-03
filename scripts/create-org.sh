dde32e9f46755bf5ec61fccb100b2d35e1a5342b
jenishnj <65988674+jenishnj@users.noreply.github.com>
2022-11-03 10:48:51 +0530

    Update create-org.sh

--- scripts/create-org.sh
+++ scripts/create-org.sh
@@ -1,15 +1,16 @@
 #!/bin/bash 
-echo 'Hello World'
-echo $@
 
-LOGIN_USERNAME='test-xr8i0fahlfqc@example.com'
+# Create scratch org
+userDetails=$(sfdx force:org:create -f config/project-scratch-def.json -s --json -d 1 -w 10)
+LOGIN_USERNAME=$(echo $userDetails | jq -r .result.username)
+# LOGIN_USERNAME='test-xr8i0fahlfqc@example.com'
 
 # Generate password
-# sfdx force:user:password:generate --targetusername $LOGIN_USERNAME
+sfdx force:user:password:generate --targetusername $LOGIN_USERNAME
 
 # Get scratch org details
-# orgDetails=$(sfdx force:org:display -u $LOGIN_USERNAME --json)
-# echo $orgDetails | grep -i password
+sfdx force:org:display -u $LOGIN_USERNAME --json >> temp.json
+
 LOGIN_PASSWORD=$(cat temp.json | awk '/password/ {print $2}' | sed 's/[",]//g')
 LOGIN_INSTANCE_URL=$(cat temp.json | awk '/instanceUrl/ {print $2}' | sed 's/[",]//g')
 LOGIN_ORG_ID=$(cat temp.json | awk '/id/ {print $2;exit}' | sed 's/[",]//g')
